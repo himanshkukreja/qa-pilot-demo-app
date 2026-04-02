8637cd88543c4de8ebe8affaef8650343f2ed5f4
Himanshu Kukreja <himansharora2001@gmail.com>
2026-04-02 12:40:19 +0530

    fix: update Vite configuration for server and preview settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,17 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  
   server: {
     port: 3000,
     open: true,
+    host: true, // allows external access
+    allowedHosts: ['demo.himanshukukreja.in'],
   },
+
   preview: {
     port: 3000,
+    host: true,
+    allowedHosts: ['demo.himanshukukreja.in'],
   },
 })
